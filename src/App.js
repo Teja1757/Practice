bff76742d817576f2eafe767563b1f8bc831f224
Teja1757 <137250558+Teja1757@users.noreply.github.com>
2024-10-18 13:49:40 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import '@patternfly/react-core/dist/styles/base.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { LoginPageHideShowPassword } from './components/PatternLogin.tsx';
-import { PageWithOrWithoutFill } from './components/Dashboard.tsx';
+import { Loginpage } from './components/Loginpage.tsx';
+import { Home } from './components/Home.tsx';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPageHideShowPassword />} />
-        <Route path="/page1" element={<PageWithOrWithoutFill />} />
+        <Route path="/" element={<Loginpage />} />
+        <Route path="/home/*" element={<Home />} />
+        
       </Routes>
     </Router>
   );
---
+++ src/components/Home.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Dashboard } from './Dashboard.tsx';
+import {Navbar} from './Navbar.tsx';
+import { Routes, Route } from 'react-router-dom';
+import {Page, PageSection} from '@patternfly/react-core';
+export const Home: React.FunctionComponent  = () => 
+{
+    
+    return(
+        <Page header={<Navbar/>}>
+        <PageSection>
+        <Routes>
+        <Route path="/home" element={<div>Welcome to Home Page</div>} /> {/* Home default view */}
+          <Route path="dashboard" element={<Dashboard />} /> {/* Route for Dashboard */}
+          <Route path="checkpoint" element={<div>CheckPoint Page</div>} />
+          <Route path="paloalto" element={<div>Palo Alto Page</div>} />
+          <Route path="forti" element={<div>Forti Page</div>} />
+          <Route path="ciscoasa" element={<div>Cisco ASA Page</div>} />
+          <Route path="userinfo" element={<div>User Information Page</div>} />
+        </Routes>
+        </PageSection>
+        </Page>
+    );
+};
