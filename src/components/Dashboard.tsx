ce58ad4582d8ed05d3470738c8af301b7ad785ea
Teja1757 <137250558+Teja1757@users.noreply.github.com>
2024-10-18 13:51:49 +0530

    Add files via upload
    This is loginwithhome

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Dashboard: React.FunctionComponent  = () => 
+{
+    return (
+        <>
+        <h1> This is the dashboard</h1>
+        </>
+    );
+};
